b35d4aa06ec52f212c4b4e5ec1b84ee4333660b3
Thomas Kazak <tjkazak@gmail.com>
2017-11-02 11:33:33 -0400

    Added try catch and custom exceptions test

---
+++ 0.16_TryCatchNew/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._16_TryCatchNew
+{
+    class Program
+    {
+        static void ProcessString(string s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException();
+            }
+            throw new Exception();
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string s = "Tom";
+                ProcessString(s);
+            }
+            // Most specific: 
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("{0} First exception caught.", e);
+            }
+            // Least specific: 
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Second exception caught.", e);
+            }
+
+        }
+    }
+}
