40c2e4b18459c7f944c240367086e8968e6d975d
Thomas Kazak <tjkazak@gmail.com>
2017-11-02 10:24:59 -0400

    Added part 2 of quiz

--- QuizOneAnswer/Program.cs
+++ QuizOneAnswer/Program.cs
@@ -22,6 +22,29 @@ namespace QuizOneAnswer
             triFour.Area();
             triFive.Area();
 
+            List<Triangle> tAreaTwo = new List<Triangle>()
+            {
+                //Objects
+                new Triangle {TBase = 2, THeight = 3},
+                new Triangle {TBase = 4, THeight = 3},
+                new Triangle {TBase = 6, THeight = 3},
+                new Triangle {TBase = 8, THeight = 3}
+
+            };
+
+            //Set up the query
+            List<Triangle> query = from t in tAreaTwo
+                                             where t.TArea > 5
+                                             orderby t.Area ascending
+                                             select t;
+
+            foreach (Triangle triangle in query)
+            {
+                Console.WriteLine(triangle.TArea);
+
+            }
+
+
         }
     }
 }
