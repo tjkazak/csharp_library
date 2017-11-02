0cbeb17620534e2f09a91e77074d2d3738813505
Thomas Kazak <tjkazak@gmail.com>
2017-11-02 09:58:04 -0400

    Added Quiz 1 Answers

--- QuizOne/Triangle.cs
+++ QuizOne/Triangle.cs
@@ -9,17 +9,17 @@ namespace QuizOne
     class Triangle : Shape
     {
         //Constructor
-        public Triangle()
+        public Triangle(int TBase, int THeight)
             {
+
             }
 
         public int TBase { get; set; }
         public int THeight { get; set; }
+        public int TArea { get; set; }
 
- 
         public override double Area()
         {
-
             return (TBase * THeight) / 2;
         }
 
